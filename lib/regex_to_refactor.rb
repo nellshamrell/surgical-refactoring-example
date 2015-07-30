2b32fa761a80f7e8cb499f4ad9824e141b7cd572
Nell Shamrell <nellshamrell@gmail.com>
2015-07-30 15:21:49 -0700

    first regex refactoring - 6

--- lib/regex_to_refactor.rb
+++ lib/regex_to_refactor.rb
@@ -3,7 +3,11 @@ class RegexToRefactor
   # Regex is a sub program within a program, so we separate it out to concentrate just on it first
   # Same as refactoring "normal code" - we do it piece by piece
   def first_regex_match(string)
-    /:([[:alpha:]]+[[:space:]])/.match(string)
+    # Resist the urge to make changes as you copy the regex over
+    # right now we're just trying to figure out what it does!
+    # we'll optimize later
+    # Regex is a good "seam" - program within a program
+    /:([[:alpha:]]+[[:space:]])=>/.match(string)
   end
 
   def scary_regex_command(directory)
