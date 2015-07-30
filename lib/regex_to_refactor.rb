9a98d1d6497cad3ecd06cb000c1ce0f606a10cb0
Nell Shamrell <nellshamrell@gmail.com>
2015-07-29 17:22:22 -0700

    clean start for refactoring

--- lib/regex_to_refactor.rb
+++ lib/regex_to_refactor.rb
@@ -1,10 +1,5 @@
 class RegexToRefactor
   def self.scary_regex_command(directory)
- #   exec "sed -i -r 's/:(\w+)\s=>/\1:/g' #{directory}/**/*.rb"
-
- #   system "sed -i '' 's/:(\w+)\s=>/\1:/g' #{directory}/**/*.rb"
     system "sed -E -i '' 's/:([[:alpha:]]+)[[:space:]]=>/\\1:/g' #{directory}/**/*.rb"
   end
 end
-
-RegexToRefactor.scary_regex_command('test_files')
--- spec/regex_to_refactor_spec.rb
+++ spec/regex_to_refactor_spec.rb
@@ -1,7 +1,4 @@
 require_relative '../lib/regex_to_refactor'
 
 describe RegexToRefactor do
-  it 'does something' do
-    RegexToRefactor.scary_regex_command('test_files')
-  end
 end
