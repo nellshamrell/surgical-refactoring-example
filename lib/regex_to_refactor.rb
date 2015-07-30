278f4bdb0859a74e4541c440fa23f9ba6379e6b7
Nell Shamrell <nellshamrell@gmail.com>
2015-07-30 16:23:58 -0700

    verifying the first flag passed to the sed command

--- lib/regex_to_refactor.rb
+++ lib/regex_to_refactor.rb
@@ -1,8 +1,16 @@
 class RegexToRefactor
   def scary_regex_command(directory)
-    # Now we notice the g flag at the end of the substitution command
-    # This normally means to replace all matches for the first match
-    # Let's write a test to verify, make it fail, then make it pass again
+    # We're almost there, now we need to add tests to verify
+    # what each of the flags do.  Let's start with the -E flag
+    # According to the man page for sed, the -E makes it so
+    # sed can use extended (modern) regular expressions, rather than basic regular expressions
+    # let's try taking that flag out and seeing
+    # if any of our current tests break - is it necessary for the regex that we are using?
+    # This breaks several tests, most notably the one that
+    # tests whether the system command returns true
+    # since it doesn't that means this causes an error with the sed method
+    # Let's add the flag back in, our existing tests already
+    # cover this behavior since they broke when we removed the flag
     system "sed -E -i '' '#{substitute_command}' #{directory_path(directory)}"
   end
 
